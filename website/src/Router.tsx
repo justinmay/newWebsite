c61896e6fa4e3fcfcffc5e202e33834a0f9cecba
Jmay <justin.sean.may@gmail.com>
2019-08-11 15:58:32 -0700

    added router, working with TS

---
+++ website/src/Router.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from './components/Home'
+
+
+interface RouterProps {
+}
+
+const SiteRouter: React.SFC<RouterProps> = (props) => {
+    return(
+    <Router>
+        <div>  
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Vineeth" component={Home} />
+        </div>
+    </Router>);
+}
+
+export default SiteRouter
